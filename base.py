3e4aa3e81a16f9cb21f46411f37abd3621fb0592
Your Name <you@example.com>
2022-10-07 18:32:28 +0300

    "Пт 07 окт 2022 18:32:28" -

--- base.py
+++
@@ -1,9 +0,0 @@
-import chardet
-
-from pathlib import Path
-
-def get_encoding(path:str)->str:
-    """
-    Получить кодировку файла, чтобы его можно было коректно прочитать
-    """
-    return chardet.detect(Path(path).read_bytes()).get('encoding')
---
+++ out_sql.sql
@@ -0,0 +1,12 @@
+    SCHID                    --  id услуги
+    ,KODOPER                 -- код услуги
+    ,SCHNAME                 -- имя услуг
+    ,SNAME                   -- специальность услуг
+    ,count(SCHID) as s_count -- количество услуг 
+    ,sum(COST_US) as s_sum   -- сумма услуг
+                           ----- DEBUG ----------------------------
+    ,list(PRICE_P_RUB) as PRICE_P_RUB
+    ,list(SCOUNT) as SCOUNT
+    ,list(HOW_GET_WSHEMA) as HOW_GET_WSHEMA
+    ,list(IN_DATE) as IN_DATE
+    ,list(FULLNAME) as FULLNAME
